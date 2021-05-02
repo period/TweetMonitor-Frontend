877c5156d0fa219950e2bc545298471bc5f90e08
Thomas <chearful@chearfulcraft.org>
2021-05-02 20:58:24 +0100

    Display tweets in table-structure with chart

---
+++ components/TweetRow.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="row">
+        <div class="col-4">
+            {{ tweet.text }}
+        </div>
+        <div class="col-1">
+            {{ tweet.metrics[0].impressions }}
+        </div>
+        <div class="col-7">
+            <line-chart style="height: 300px;" :data="{
+                labels: tweet.metrics.map((el) => { return new Date(parseInt(el.timestamp)) }),
+                datasets: [
+                    {
+                        label: 'Impressions',
+                        data: tweet.metrics.map((el) => { return el.impressions }),
+                        borderColor: '#1DA1F2',
+                        backgroundColor: 'rgba(0,0,0,0)'
+                    }
+                ]}"
+            :options="{
+                responsive: true,
+                spanGaps: true, 
+                maintainAspectRatio: false, 
+                scales: {
+                    xAxes: [
+                        {type:'time'}
+                    ]
+                }
+            }" />
+        </div>
+    </div>
+</template>
+<style scoped>
+.row {
+    border-top: 1px solid #ccd6dd;
+}
+</style>
+<script>
+import LineChart from "~/components/LineChart";
+export default {
+    name: "TweetRow",
+    components: {LineChart},
+    props: {
+        tweet: {
+            type: Object,
+            default: {}
+        }
+    },
+    data() {
+        return {
+        }
+    },
+    mounted() {
+    }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,32 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        TweetMonitor-FrontEnd
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+    <div class="container">
+        <h1 class="text-center">Tweetlytics</h1>
+
+        <div class="row">
+            <div class="offset-md-6 col-2"><strong>Impressions</strong></div>
+        </div>
+        <tweet-row v-for="tweet in tweets" :tweet="tweet" :key="tweet.id" />
     </div>
-  </div>
 </template>
 
 <script>
-export default {}
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import TweetRow from "~/components/TweetRow.vue";
+
+export default {
+    name: "Index",
+    components: {TweetRow},
+    data() {
+        return {
+            tweets: []
+        }
+    },
+    async mounted() {
+        this.$axios.get("https://tweetlytics.thomas.gg/api/tweets", {}).then((res) => {
+            this.tweets = res.data;
+            console.log(this.tweets[0].metrics)
+        })
+    }
+}
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
