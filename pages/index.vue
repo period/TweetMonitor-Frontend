470afdd8e2fbbccc653f674344caf5268682b047
Thomas <chearful@chearfulcraft.org>
2021-05-02 22:03:38 +0100

    Infinite scrolling, filtering

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,18 @@
 <template>
     <div class="container">
         <h1 class="text-center">Tweetlytics</h1>
-
         <div class="row">
-            <div class="offset-md-6 col-2"><strong>Impressions</strong></div>
+            <div class="offset-lg-9 col-3">
+                <div class="input-group">
+                    <input type="number" class="form-control" v-model="minimum_impressions" placeholder="Min views">
+                    <div class="input-group-append">
+                        <button class="btn btn-outline-secondary" @click="filter">Filter</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="offset-md-4 col-2"><strong>Impressions</strong></div>
         </div>
         <tweet-row v-for="tweet in tweets" :tweet="tweet" :key="tweet.id" />
     </div>
@@ -19,14 +28,34 @@ export default {
     components: {TweetRow},
     data() {
         return {
-            tweets: []
+            tweets: [],
+            minimum_impressions: null,
         }
     },
     async mounted() {
-        this.$axios.get("https://tweetlytics.thomas.gg/api/tweets", {}).then((res) => {
-            this.tweets = res.data;
-            console.log(this.tweets[0].metrics)
-        })
+        window.addEventListener("scroll", this.onScroll);
+        this.loadTweets();
+    },
+    beforeDestroy() {
+        window.removeEventListener("scroll", this.onScroll);
+    },
+    methods: {
+        filter() {
+            this.tweets = [];
+            this.loadTweets();
+        },
+        async loadTweets() {
+            let params = {};
+            if(this.tweets.length != 0) params.offset = this.tweets[this.tweets.length-1].created_at;
+            if(this.minimum_impressions != null && this.minimum_impressions > 0) params.minimum_impressions = this.minimum_impressions;
+            this.$axios.get("https://tweetlytics.thomas.gg/api/tweets", {params: params}).then((res) => {
+                for(let tweet of res.data) this.tweets.push(tweet);
+            })
+        },
+        async onScroll() {
+            let bottomOfWindow = Math.max(window.pageYOffset, document.documentElement.scrollTop, document.body.scrollTop) + window.innerHeight === document.documentElement.offsetHeight
+            if(bottomOfWindow) this.loadTweets();
+        }
     }
 }
 </script>
